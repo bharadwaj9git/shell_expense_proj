2b19a7b3ed8a19f1de15ebd156fecb83ed48ccaf
MYPC <bharadwajsai567@gmail.com>
2024-05-25 20:34:32 +0530

    front end config file

--- backend.sh
+++ backend.sh
@@ -1,7 +1,7 @@
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
-useradd expense
+useradd shell_expense
 cp backend.service /etc/systemd/system/backend.service
 mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
