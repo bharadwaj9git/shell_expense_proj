9890b3f44b99b9b5618e35118b37580198977575
MYPC <bharadwajsai567@gmail.com>
2024-05-25 23:32:27 +0530

    variable

--- mysql.sh
+++ mysql.sh
@@ -9,4 +9,4 @@ echo -e "\e[33m start service \e[0m"
 systemctl start mysqld
 
 echo -e "\e[33m setting credentials \e[0m"
-mysql_secure_installation --set-root-pass ExpenseApp@1
+mysql_secure_installation --set-root-pass $1
