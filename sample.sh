270877ed2b75aaa325722005adc13af28fbf25d0
MYPC <bharadwajsai567@gmail.com>
2024-05-25 21:19:28 +0530

    colors

--- sample.sh
+++ sample.sh
@@ -1,8 +1,8 @@
 echo hello world
 
-echo -e"\e[31m hello world \e[0m "
-echo -e"\e[32m hello world \e[0m "
-echo -e"\e[330m hello world \e[0m "
-echo -e"\e[34m hello world \e[0m "
-echo -e"\e[35m hello world \e[0m "
-echo -e"\e[36m hello world \e[0m "
+echo -e "\e[31m hello world \e[0m"
+echo -e "\e[32m hello world \e[0m"
+echo -e "\e[330m hello world \e[0m"
+echo -e "\e[34m hello world \e[0m"
+echo -e "\e[35m hello world \e[0m"
+echo -e "\e[36m hello world \e[0m"
