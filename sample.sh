cf703b2df5663b136bba741a66d033b35bcc098f
MYPC <bharadwajsai567@gmail.com>
2024-05-25 22:23:52 +0530

    variable

--- sample.sh
+++ sample.sh
@@ -8,8 +8,16 @@ echo -e "\e[35m hello world \e[0m"
 echo -e "\e[36m hello world \e[0m"
 
 # variable declaration and accessing variable
-
+#user defined variable
 x=10
 echo x value is $x
 echo x value is ${x}
 
+#system variables
+# $0 $1 $2 .....$n
+# $#,$*,$?
+
+# $? it will give exit status
+
+ls
+echo exit state - $?
