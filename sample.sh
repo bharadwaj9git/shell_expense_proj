ce7873cbace6ac67b84645f323c486f1c4a81159
MYPC <bharadwajsai567@gmail.com>
2024-05-25 23:07:30 +0530

    variable

--- sample.sh
+++ sample.sh
@@ -1,3 +1,4 @@
+# printing
 echo hello world
 
 echo -e "\e[31m hello world \e[0m"
@@ -23,3 +24,20 @@ ls
 echo exit status - $?
 # exit state 0 -success
 # 1-255 failure
+
+# redirectors
+
+echo -e "\e[31m create output log file \e[0m"
+touch opt
+echo -e "\e[31m sending ls output to log file \e[0m"
+ls > opt
+echo -e "\e[31m viewing content in log file \e[0m"
+cat opt
+
+
+# "<" input ">" output
+#  2> to store error output
+# &> it will combine both o/p and error o/p
+# >> will append content, so we use &>>   ex: dnf install nginx -y &>> /tmp/expense.log will use in our porject
+
+
