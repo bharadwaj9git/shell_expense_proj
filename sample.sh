8d860d7fa3da04478194acf05a9882370be29fb3
MYPC <bharadwajsai567@gmail.com>
2024-05-25 22:17:53 +0530

    variable

--- sample.sh
+++ sample.sh
@@ -6,3 +6,10 @@ echo -e "\e[33m hello world \e[0m"
 echo -e "\e[34m hello world \e[0m"
 echo -e "\e[35m hello world \e[0m"
 echo -e "\e[36m hello world \e[0m"
+
+# variable declaration and accessing variable
+
+x=10
+
+echo x is $x
+
