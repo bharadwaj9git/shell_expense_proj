14cd6bebb6c7a7f9ec05844f5309e583a88f1dff
MYPC <bharadwajsai567@gmail.com>
2024-05-25 20:39:15 +0530

    back end changes

--- backend.sh
+++ backend.sh
@@ -1,11 +1,11 @@
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
-useradd shell_expense
+useradd shell_prj
 cp backend.service /etc/systemd/system/backend.service
-mkdir /app
+mkdir /apps
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
-cd /app
+cd /apps
 unzip /tmp/backend.zip
 npm install
 dnf install mysql -y
