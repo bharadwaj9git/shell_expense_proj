30c73fc1aec93394a5ecb7508c1c33557884fb72
MYPC <bharadwajsai567@gmail.com>
2024-05-26 12:55:31 +0530

    variable

--- frontend.sh
+++ frontend.sh
@@ -14,32 +14,74 @@ fi
 
 heading config file copy
 cp frontend.conf /etc/nginx/default.d/frontend.conf &>> /tmp/expense.log
-echo exit status - $?
+if [ $? -eq 0 ]; then
+echo success
+else
+echo failure
+exit 2
+fi
+
 
 heading removing default content
 rm -rf /usr/share/nginx/html/*  &>> /tmp/expense.log
-echo exit status - $?
+if [ $? -eq 0 ]; then
+echo success
+else
+echo failure
+exit 2
+fi
+
 
 heading downloading our own content
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip &>> /tmp/expense.log
-echo exit status - $?
+if [ $? -eq 0 ]; then
+echo success
+else
+echo failure
+exit 2
+fi
+
 
 heading change path to nginx html
 # shellcheck disable=SC2164
 cd /usr/share/nginx/html &>> /tmp/expense.log
-echo exit status - $?
+if [ $? -eq 0 ]; then
+echo success
+else
+echo failure
+exit 2
+fi
+
 
 heading unzip content
 unzip /tmp/frontend.zip &>> /tmp/expense.log
-echo exit status - $?
+if [ $? -eq 0 ]; then
+echo success
+else
+echo failure
+exit 2
+fi
+
 
 heading restarting service
 systemctl restart nginx &>> /tmp/expense.log
-echo exit status - $?
+if [ $? -eq 0 ]; then
+echo success
+else
+echo failure
+exit 2
+fi
+
 
 heading enabling service
 systemctl enable nginx &>> /tmp/expense.log
-echo exit status - $?
+if [ $? -eq 0 ]; then
+echo success
+else
+echo failure
+exit 2
+fi
+
 
 
 
--- mysql.sh
+++ mysql.sh
@@ -7,12 +7,39 @@ fi
 
 heading installing mysql server
 dnf install mysql-server -y
+if [ $? -eq 0 ]; then
+echo success
+else
+echo failure
+exit 2
+fi
+
 
 heading enabling service
 systemctl enable mysqld
+if [ $? -eq 0 ]; then
+echo success
+else
+echo failure
+exit 2
+fi
+
 
 heading start service
 systemctl start mysqld
+if [ $? -eq 0 ]; then
+echo success
+else
+echo failure
+exit 2
+fi
+
 
 heading setting credentials
 mysql_secure_installation --set-root-pass $1
+if [ $? -eq 0 ]; then
+echo success
+else
+echo failure
+exit 2
+fi
